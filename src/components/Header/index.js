f85f6b8b8ae965dd35a63b89161bb7e900f7cdb8
Ryan Jordan <Ryan@geoso.io>
2021-03-24 08:50:36 -0500

    Updated resume link and fixed header on scroll background.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './header.css';
 
 class Header extends Component {
+  state = {
+    color: 'transparent'
+  }
+
+  listenScrollEvent = e => {
+    console.log(window.scrollY);
+    if (window.scrollY > 30) {
+      this.setState({color: '#2B2B2B'})
+    } else {
+      this.setState({color: 'transparent'})
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent)
+  }
+
   render() {
 
     if(this.props.data){
@@ -13,7 +30,7 @@ class Header extends Component {
     return (
       <header id="home">
         <nav id="nav-wrap">
-          <ul id="nav" className="nav">
+          <ul id="nav" className="nav" style={{backgroundColor: this.state.color}}>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
   	        <li><a href="#resume">Resume</a></li>
